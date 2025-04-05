31f8a2759e7f22001ff172f92f8c2356b4b2a9de
ayaan <asm.ayyaan@gmail.com>
2025-04-05 07:50:33 -0700

    Extending Blade

---
+++ resources/views/about.blade.php
@@ -0,0 +1,25 @@
+@extends('layouts.app')
+
+@section('contents')
+
+<div class="pricing-header p-3 pb-md-4 mx-auto text-center">
+    <h1 class="display-4 fw-normal">About Page</h1>
+    <p class="fs-5 text-muted">Quickly build an effective pricing table for your potential customers with
+        this Bootstrap example. It’s built with default Bootstrap components and utilities with little
+        customization.
+        Quickly build an effective pricing table for your potential customers with
+        this Bootstrap example. It’s built with default Bootstrap components and utilities with little
+        customization
+        Quickly build an effective pricing table for your potential customers with
+        this Bootstrap example. It’s built with default Bootstrap components and utilities with little
+        customization
+        Quickly build an effective pricing table for your potential customers with
+        this Bootstrap example. It’s built with default Bootstrap components and utilities with little
+        customization
+        Quickly build an effective pricing table for your potential customers with
+        this Bootstrap example. It’s built with default Bootstrap components and utilities with little
+        customization
+    </p>
+</div>
+
+@endsection
