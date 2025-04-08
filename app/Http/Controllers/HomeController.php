7d6bc9d8c40ab0151ced9cf0a9bf560fc2619d75
ayaan <asm.ayyaan@gmail.com>
2025-04-07 22:18:02 -0700

    Model

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,11 +2,14 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Post;
 use Illuminate\Http\Request;
 
 class HomeController extends Controller
 {
     function index() {
+        $posts = Post::all();
+        echo $posts;
         return view('home');
     }
 
