f9a8e9bde532d6c55d0950fd3ca6657982af08f9
ayaan <asm.ayyaan@gmail.com>
2025-04-11 16:00:21 -0700

    Eloquent - Creating New Data

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use App\Http\Controllers\AboutController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HomeController;
+use App\Models\Blog;
 
 // Route::get('/', function () {
 //     return view('welcome');
@@ -12,3 +13,15 @@ use App\Http\Controllers\HomeController;
 Route::get('/', [HomeController::class, 'index'])->name('home');
 
 Route::get('/about', [AboutController::class, 'about'])->name('about');
+
+Route::get('/blog/create', function() {
+    // create data with eloquent orm
+    $blog = new Blog();
+    $blog->title = 'this is our first blog 2';
+    $blog->description = 'this is a test descripiton 2';
+    $blog->image = '/uploads/test.php';
+    $blog->save();
+
+    return $blog;
+
+});
