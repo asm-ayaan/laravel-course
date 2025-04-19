12eb363390ed441a0371fd6edf4967d0a46fac57
ayaan <asm.ayyaan@gmail.com>
2025-04-19 16:23:48 -0700

    Form Validation

--- app/Http/Controllers/FormController.php
+++ app/Http/Controllers/FormController.php
@@ -11,6 +11,12 @@ class FormController extends Controller
     }
 
     function store(Request $request) {
+        $request->validate([
+            // 'title' => 'required|min:5|max:20',
+            'title' => ['required', 'min:5', 'max:20'],
+
+            'description' => 'required|min:5|max:2000'
+        ]); 
         return $request;
     }       
 }
--- resources/views/form/index.blade.php
+++ resources/views/form/index.blade.php
@@ -2,6 +2,13 @@
 @section('contents')
     <div>
        <div class="col-md-4">
+
+        {{-- @if($errors->any())
+            @foreach($errors->all() as $error)
+                <div class="alert alert-danger">{{ $error }}</div>
+            @endforeach
+        @endif --}}
+
         <div class="card">
             <div class="card-header">
                 <h4>Form</h4>
@@ -13,10 +20,16 @@
                     <div class="form-group">
                         <label for="">Title</label>
                         <input type="text" name="title" class="form-control">
+                        @error('title')
+                            <i class="text-danger">{{ $message }}</i>
+                        @enderror
                     </div>
                     <div>
                         <label for="">Description</label>
                         <textarea name="description" class="form-control"></textarea>
+                        @error('description')
+                                <i class="text-danger">{{ $message }}</i>
+                        @enderror
                     </div>
                     <button type="submit" class="btn btn-primary mt-2">Submit</button>
                 </form>
