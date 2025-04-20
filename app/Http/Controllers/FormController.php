1f6230fc8f54b4fe0847f688a7e94dc5a5e6aee6
ayaan <asm.ayyaan@gmail.com>
2025-04-19 17:04:22 -0700

    Custom Validation Message

--- app/Http/Controllers/FormController.php
+++ app/Http/Controllers/FormController.php
@@ -13,9 +13,13 @@ class FormController extends Controller
     function store(Request $request) {
         $request->validate([
             // 'title' => 'required|min:5|max:20',
-            'title' => ['required', 'min:5', 'max:20'],
+            'title' => ['required', 'min:20', 'max:20'],
 
             'description' => 'required|min:5|max:2000'
+        ], [
+            'title.required' => 'Hey your are missing this field',
+            'title.min' => 'title must be getter then :min careacters',
+            'title.max' => 'title must be less then :max careacters', 
         ]); 
         return $request;
     }       
